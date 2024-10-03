758df46de5b12b974353e2b90e063be0199c17f4
Nicholas Bloor <161645384+nbloor@users.noreply.github.com>
2024-10-02 19:38:34 -0500

    Update app.py

--- app.py
+++ app.py
@@ -3,3 +3,6 @@ import streamlit as st
 st.title("Task Manager Project")
 
 st.write("Welcome to the Task Manager Program.")
+
+
+st.text_input("Write the name of the task that needs to be performed here.")
