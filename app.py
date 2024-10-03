2fd7ea406355d57d720c64247065fad0a182519e
Nicholas Bloor <161645384+nbloor@users.noreply.github.com>
2024-10-02 19:49:09 -0500

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,9 @@ st.write("Welcome to the Task Manager Program.")
 
 name_of_task = st.text_input("Write the name of the task that needs to be performed here:")
 date_task_due = st.date_input("The date that the task must be performed by:")
-is_clicked = st.button("Add task to the list of required tasks")
+add_task = st.button("Add task to the list of required tasks")
+remove_task = st.button("Cick this to remove a task from the task manager")
+kill_task_manager = st.button("Click this to end the program")
 
 st.write("Tasks To Do:")
 
