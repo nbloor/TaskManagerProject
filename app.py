b79953ba56a2eb4044a9ea7d0b60bd1fd6c09188
Nicholas Bloor <161645384+nbloor@users.noreply.github.com>
2024-10-02 19:41:42 -0500

    Update app.py

--- app.py
+++ app.py
@@ -7,3 +7,4 @@ st.write("Welcome to the Task Manager Program.")
 
 name_of_task = st.text_input("Write the name of the task that needs to be performed here:")
 date_task_due = st.date_input("The date that the task must be performed by:")
+st.button("Click me")
